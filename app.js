b43d744e9c0346c51a677493e89cb8e3cd2e8152
James Pierce <jamespierce@fastmail.com>
2020-06-21 11:51:58 -0500

    view updates when wrong answer

--- app.js
+++ app.js
@@ -8,20 +8,50 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-app.action(/^correct_answer_.*$/, async({context, body, ack}) => {
+app.action(/^correct_answer_.*$/, async ({context, body, ack}) => {
   await ack();
-  console.log(body);
+  console.log(body.view.blocks.find(block => block.type == 'actions'));
 });
 
-app.action(/^incorrect_answer_.*$/, async({context, body, ack}) => {
+app.action(/^incorrect_answer_.*$/, async ({context, body, ack}) => {
   await ack();
-  console.log(body);
+
+  const buttonsIndex = body.view.blocks.findIndex(
+    block => block.type == 'actions',
+  );
+  const buttons = body.view.blocks.find(block => block.type == 'actions');
+  const clickedButton = buttons.elements.findIndex(
+    button => button.action_id == context.actionIdMatches[0],
+  );
+  const newViewBlocks = body.view.blocks;
+  newViewBlocks[0] = {
+    type: 'image',
+    image_url: body.view.blocks[0].image_url,
+    alt_text: body.view.blocks[0].alt_text,
+  };
+  const text = body.view.blocks[buttonsIndex].elements[clickedButton].text.text
+  newViewBlocks[buttonsIndex].elements[clickedButton].style = 'danger';
+  newViewBlocks[buttonsIndex].elements[clickedButton].text.text = `❌ ${text}`;
+  try {
+    const result = await app.client.views.update({
+      token: context.botToken,
+      view_id: body.view.id,
+      view: {
+        type: 'modal',
+        title: body.view.title,
+        blocks: newViewBlocks,
+      },
+    });
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.action('study_hiragana', async ({context, body, ack, say}) => {
   console.log('study_hiragana fired');
   await ack();
-  
+
   try {
     const result = await app.client.views.open({
       token: context.botToken,
